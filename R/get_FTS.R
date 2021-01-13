04ff3cec55728757319a38dbab8ad70bffe63a0c
edouard-legoupil <edouard.legoupil@gmail.com>
2021-01-13 10:39:06 -0500

    rev fts

--- R/get_FTS.R
+++ R/get_FTS.R
@@ -27,12 +27,12 @@ get_FTS <- function(url = "https://api.hpc.tools",
                     boundary = "year=2020",
                     #filter = "&destinationCountryISO3:ABW,AIA,ARG,ASM,ATG,BES,BHS,BLM,BLZ,BMU,BOL,BRA,BRB,CAN,CHL,COL,CRI,CUB,CUW,CYM,DMA,DOM,ECU,FLK,GLP,GRD,GTM,GUF,GUY,HND,HTI,JAM,KNA,LCA,MAF,MEX,MSR,MTQ,NFK,NIC,PAN,PER,PRI,PRY,SGS,SLV,SPM,SUR,SXM,TCA,TTO,URY,USA,VCT,VEN,VGB,VIR",
                     #grouping = "&groupby=country",
-                    limit = "&limit=1000"
+                    limit = "&limit=1000&page=1"
 )
 {
 
-  reference <- unhcrdatapackage::reference
-  as.character(reference[ reference$UNHCRBureau == "Americas", c("iso_3")])
+  #reference <- unhcrdatapackage::reference
+  #as.character(reference[ reference$UNHCRBureau == "Americas", c("iso_3")])
   
   #ABW,AIA,ARG,ASM,ATG,BES,BHS,BLM,BLZ,BMU,BOL,BRA,BRB,CAN,CHL,COL,CRI,CUB,CUW,CYM,DMA,DOM,ECU,FLK,GLP,GRD,GTM,GUF,GUY,HND,HTI,JAM,KNA,LCA,MAF,MEX,MSR,MTQ,NFK,NIC,PAN,PER,PRI,PRY,SGS,SLV,SPM,SUR,SXM,TCA,TTO,URY,USA,VCT,VEN,VGB,VIR
   
@@ -84,32 +84,108 @@ get_FTS <- function(url = "https://api.hpc.tools",
   # get data as binary from the specified url using RCurl
   #data <- RCurl::getURL(utils::URLencode(full_url))
   
-  
+  ## API Call ######
   # encode the URL to replace all spaces with the ascii value for a space which is %20.
   # and Convert JSON to data frame
   ftsflow <- rjson::fromJSON(RCurl::getURL(utils::URLencode(paste0(url,
                                                                     path))
                                            ))
   
-  #names(ftsflow$data$flows)
   
-  if ( !(is.na(grouping))) {
-    flows <- ftsflow$data
-  } else {
-    flows <- ftsflow$data$flows
-  }
+  ### Tidy Results ##########
+  #names(ftsflow$data$flows)
+  # if ( !(is.na(grouping))) {
+  #   flows <- ftsflow$data
+  # } else { }
+  flows <- ftsflow$data$flows
+  
   #str(flows)
   
-  #flowsreport1 <- ftsflow$data$report1$fundingTotals$objects
-  #flows1 <- data.frame(Reduce(rbind, report1funding))
+  flows_frame <- data.frame(Reduce(rbind, flows))
   
+  colflow <- c( "id", "amountUSD", "budgetYear" , "childFlowIds" ,             
+                "contributionType", "createdAt", "date", "decisionDate",              
+                "description" , "grandBargainEarmarkingType", "exchangeRate", 
+                "firstReportedDate" , "flowType", "keywords", "newMoney" ,
+                "originalAmount", "originalCurrency" , "method"  ,
+                "parentFlowId" , "status" , "updatedAt", "versionId" , 
+                "sourceObjects", "destinationObjects",
+                "boundary", "onBoundary", "reportDetails", "refCode" )
   
+  flows_frame0 <- tidyr::unnest_longer(flows,colflow)
+  
+  ## other test to convert list to dataframe 
   # df <- data.frame(matrix(unlist(ftsflow), nrow=length(flows), byrow=T))
   # df2 <- do.call(rbind.data.frame, flows)
   # df3 <- plyr::ldply (flows, data.frame)
   # df5 <- tibble::enframe(flows)
-  flows_frame <- data.frame(Reduce(rbind, flows))
+  #flows_framet <- data.frame(Reduce(dplyr::bind_rows, flows))
   
+  
+  ### Check if there's more.... 
+  querysize <- ftsflow$meta$count
+  
+  if( querysize > 1000) {
+    pagenum <- (querysize / 1000)+1
+    cat(paste0("The query output is ",querysize ," and exceeds the API limit. We will perform multiple call.\n"))
+    
+    for (i in 2:pagenum){
+      limit <- paste0( "&limit=1000&page=", i)
+      path <- paste0(ext, 
+                     boundary, 
+                     # filter, 
+                     # grouping,
+                     limit)
+      cat ( paste0("Call to the API #", i))
+      ftsflow1 <- rjson::fromJSON(RCurl::getURL(utils::URLencode(paste0(url,path))))
+      
+      flows_frame1 <- data.frame(Reduce(rbind, ftsflow1$data$flows))
+      
+      # names(flows_frame1)
+      
+      flows_frame <- dplyr::bind_rows(flows_frame, flows_frame1)
+      #flows_frame <- rbind(flows_frame, flows_frame1)
+      rm(flows_frame1)
+      
+    }
+    
+  }
+  names(flows_frame)
+  class(flows_frame)
+  
+  ## Build node network table..
+  flows_frame$childFlowIds
+  
+  ## Build sub frame for the 3 sublist
+  # flows_frame$sourceObjects
+  # flows_frame$destinationObjects
+  # flows_frame$reportDetails
+  
+  source <- data.frame(Reduce(rbind, flows_frame[ 1, c("sourceObjects")]))
+  source$idfts <- as.character(flows_frame[ 1, c("id")])
+  destination <- data.frame(Reduce(rbind, flows_frame[ 1, c("destinationObjects")]))
+  destination$idfts <- as.character(flows_frame[ 1, c("id")])
+  report <- data.frame(Reduce(rbind, flows_frame[ 1, c("reportDetails")]))
+  report$idfts <- as.character(flows_frame[ 1, c("id")])
+  
+  for (j in 2:nrow(flows_frame)){
+    # j<- 43
+    cat(paste0("\n Processing Row #", j))
+    source1 <- data.frame(Reduce(rbind, flows_frame[ j, c("sourceObjects")]))
+    source1$idfts <- as.character(flows_frame[ j, c("id")])
+    source <- dplyr::bind_rows(source, source1)
+    rm(source1)
+    
+    destination1 <- data.frame(Reduce(rbind, flows_frame[ j, c("destinationObjects")]))
+    destination1$idfts <- as.character(flows_frame[ j, c("id")])
+    destination <- dplyr::bind_rows(destination, destination1)
+    rm(destination1)
+    
+    report1 <- data.frame(Reduce(rbind, flows_frame[ j, c("reportDetails")]))
+    report1$idfts <- as.character(flows_frame[ j, c("id")])
+    report <- dplyr::bind_rows(report, report1)
+    rm(report1)
+  }
    
   return(flows_frame)
 
